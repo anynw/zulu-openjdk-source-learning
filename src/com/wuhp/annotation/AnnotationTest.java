e32604112b5c2cd6f0f592c7a8164d5687e1c6ec
wuhuaping2006@126.com <wuhuaping2006@126.com>
2022-01-11 19:46:24 +0800

    init-project

---
+++ src/com/wuhp/annotation/AnnotationTest.java
@@ -0,0 +1,8 @@
+package com.wuhp.annotation;
+
+/**
+ * @author wuhp
+ * @date 2022/1/11
+ */
+public class AnnotationTest {
+}
---
+++ src/com/wuhp/concurrent/MapTest.java
@@ -0,0 +1,11 @@
+package com.wuhp.concurrent;
+
+/**
+ * @author wuhp
+ * @date 2022/1/11
+ */
+public class MapTest {
+    public static void main(String[] args) {
+
+    }
+}
