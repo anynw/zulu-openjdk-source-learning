499079ad912f12c7045ca88ec7af554725e5f248
wuhuaping2006@126.com <wuhuaping2006@126.com>
2022-01-15 22:30:16 +0800

    tcp client/server txt

---
+++ src/com/wuhp/net/case7/ClientDemo.java
@@ -0,0 +1,31 @@
+package com.wuhp.net.case7;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @author wuhp
+ * @date 2022/1/15
+ */
+public class ClientDemo {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 10000);
+
+        //键盘录入
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //封装输出流对象，字节输出流->字符输出流
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if ("886".equals(line)) {
+                break;
+            }
+
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+
+        socket.close();
+    }
+}
